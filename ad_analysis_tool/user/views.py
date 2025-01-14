5ab1717d1bd95f437fd96c7f7c4c97231b025b8d
sourov <sourovsh533@gmail.com>
2025-01-14 10:25:46 +0600

    path correction oftempates

--- ad_analysis_tool/user/views.py
+++ ad_analysis_tool/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+# from .forms import SignUpForm
 
 
 def home(request):
@@ -15,18 +15,18 @@ def logout_user(request):
 
 
 def register_user(request):
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # authenticate and login
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password1"]
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, "You have successfully Registered")
-            return redirect("home")
-    else:
-        form = SignUpForm()
-        return render(request, "register.html", {"form": form})
-    return render(request, "user/register.html", {"form": form})
+    # if request.method == "POST":
+    #     form = SignUpForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         # authenticate and login
+    #         username = form.cleaned_data["username"]
+    #         password = form.cleaned_data["password1"]
+    #         user = authenticate(username=username, password=password)
+    #         login(request, user)
+    #         messages.success(request, "You have successfully Registered")
+    #         return redirect("home")
+    # else:
+    #     form = SignUpForm()
+    #     return render(request, "register.html", {"form": form})
+    return render(request, "user/register.html")
