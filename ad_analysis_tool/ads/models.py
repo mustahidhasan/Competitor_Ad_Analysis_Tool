caedfc4ad7f38f983b354b52bddb3a6541be376f
Mustahid Hasan <mustahidhasan9@gmail.com>
2025-01-16 00:54:18 +0600

    models created for adds to save and configure

--- ad_analysis_tool/ads/models.py
+++ ad_analysis_tool/ads/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Configure(models.Model):
+    configure_name = models.CharField(max_length=50, null=True)
+    ACCESS_TOKEN = models.TextField(null=True)
+    APP_ID = models.CharField(max_length=200, null=True)
+    APP_SECRET = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return f'{self.configure_name} - {self.APP_ID}'
+
+class SaveRawAdsData(models.Model):
+    platform_name = models.CharField(max_length=100, null=True)
+    raw_data = models.JSONField(null=True)
+
+    def __str__(self):
+        return self.platform_name
