c62ea5730d21fb2b518f418ef26b9826a58d7cde
Mustahid Hasan <mustahidhasan9@gmail.com>
2025-02-04 16:04:57 +0600

    settings updated

--- ad_analysis_tool/ad_analysis_tool/settings.py
+++ ad_analysis_tool/ad_analysis_tool/settings.py
@@ -117,14 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+
+# Static files directory (for development)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Ensure this directory exists
+]
+
+# Static root directory (for collectstatic)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Add this line
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
